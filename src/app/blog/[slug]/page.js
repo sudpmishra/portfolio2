b6e78c87073f97bcf84e2937f9e06c49c87dd08b
sudpmishra <sudeep014@gmail.com>
2026-09-16 12:26:11 +0545

    Remove outdated blog posts on WebGL, animation, privacy, and static websites; add new article on AI's impact on junior developers.

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import BlogPost from "@/components/pages/BlogPost";
 import { getAllPostsMeta, getPostMetaBySlug } from "@/lib/blogMeta";
 
@@ -16,18 +17,31 @@ export async function generateMetadata({ params }) {
   }
 
   const url = `${siteUrl}/blog/${post.slug}`;
-  const images = post.bannerImage ? [{ url: post.bannerImage }] : undefined;
+  const images = post.bannerImage
+    ? [
+        {
+          url: post.bannerImage,
+          width: 1600,
+          height: 900,
+          alt: post.bannerAlt || post.title,
+        },
+      ]
+    : undefined;
 
   return {
     title: post.title,
     description: post.excerpt,
     alternates: { canonical: url },
+    authors: [{ name: "Sudeep Mishra", url: siteUrl }],
     openGraph: {
       type: "article",
       url,
+      siteName: "Sudeep Mishra",
+      locale: "en_US",
       title: post.title,
       description: post.excerpt,
       publishedTime: post.date || undefined,
+      authors: [siteUrl],
       tags: post.tags,
       images,
     },
@@ -36,11 +50,55 @@ export async function generateMetadata({ params }) {
       title: post.title,
       description: post.excerpt,
       images: post.bannerImage ? [post.bannerImage] : undefined,
+      creator: "@sudpmishra",
     },
   };
 }
 
 export default async function BlogPostPage({ params }) {
   const { slug } = await params;
-  return <BlogPost slug={slug} />;
+  const post = getPostMetaBySlug(slug);
+
+  if (!post) {
+    return <BlogPost slug={slug} />;
+  }
+
+  const url = `${siteUrl}/blog/${post.slug}`;
+  const articleJsonLd = {
+    "@context": "https://schema.org",
+    "@type": "BlogPosting",
+    headline: post.title,
+    description: post.excerpt,
+    image: post.bannerImage ? [post.bannerImage] : undefined,
+    datePublished: post.date,
+    dateModified: post.date,
+    author: {
+      "@type": "Person",
+      name: "Sudeep Mishra",
+      url: siteUrl,
+    },
+    publisher: {
+      "@type": "Person",
+      name: "Sudeep Mishra",
+      url: siteUrl,
+    },
+    mainEntityOfPage: {
+      "@type": "WebPage",
+      "@id": url,
+    },
+    keywords: post.tags?.join(", "),
+  };
+
+  return (
+    <>
+      <Script
+        id="blogposting-jsonld"
+        type="application/ld+json"
+        strategy="beforeInteractive"
+      >
+        {JSON.stringify(articleJsonLd)}
+      </Script>
+      <BlogPost slug={slug} />
+    </>
+  );
 }
